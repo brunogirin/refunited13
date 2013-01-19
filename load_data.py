e21aacfe25777fe07edfd6ddc54f79e575497c36
Bruno Girin <brunogirin@gmail.com>
2013-01-19 16:26:11 +0000

    Updated data loader

--- load_data.py
+++ load_data.py
@@ -1,9 +1,24 @@
-import pandas as pd
+class Data:
+    def __init__(self):
+        self.all_msg = []
+        self.good_msg = []
+        self.bad_msg = []
+        f = open("data/SMSSpamCollection", "r")
+        for line in f.readlines():
+            if line.startswith('ham '):
+                msg = line[4:]
+                self.good_msg.append(msg)
+                self.all_msg.append(msg)
+            elif line.startswith('spam '):
+                msg = line[5:]
+                self.bad_msg.append(msg)
+                self.all_msg.append(msg)
+        self.idx = 0
 
-f = open("data/SMSSpamCollection", "r")
-
-for line in f.readlines():
-
-
-data = pd.read_table("data/SMSSpamCollection")
-
+    def get_next(self):
+        if self.idx >= len(self.all_msg):
+            return None
+        else:
+            m = self.all_msg[self.idx]
+            self.idx = self.idx + 1
+            return m
