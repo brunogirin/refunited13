875a62fc1ffc81efd3882e240e522efe6b1bcad2
James Porter <j.a.porter@warwick.ac.uk>
2013-01-19 16:27:40 +0000

    gui

--- gui.py
+++ gui.py
@@ -1,13 +1,29 @@
 from Tkinter import *
+import spam
 
+data = load_data.Data()
+processor = spam.SpamProcessor()
+current_message = ""
 
 def spam():
-    pass
+    processor.flag_as_bad(current_message)
+    next()
 
 def notspam():
-    pass
+    processor.flag_as_good(current_message)
+    next()
 
 def score():
+    score = processor.score(current_message)
+    n = score[0]
+    lbl = score[1]
+    score_label.text = str(n) + " " + str(lbl)
+
+def next():
+    current_message = data.getNext()
+    score()
+
+def analyse():
     pass
 
 root = Tk()
@@ -20,19 +36,23 @@ root.minsize(640,480)
 label = Label(master=root, text="Spam(er) detection system")
 label.pack()
 
-msg = Label(master=root, text="Spam(er) detection system")
+msg = Label(master=root, text=current_message)
 msg.pack(side=TOP, fill=X, expand=1)
 
 buttonFrame = Frame(master=root)
 
 b = Button(buttonFrame, text="Spam", command=spam)
 b.pack()
-b2 = Button(buttonFrame, text="Not Spam", command=spam)
+b2 = Button(buttonFrame, text="Not Spam", command=notspam)
 b2.pack()
-b3 = Button(buttonFrame, text="Score", command=spam)
+b3 = Button(buttonFrame, text="Next", command=next)
 b3.pack()
-b4 = Button(buttonFrame, text="Analyse", command=spam)
+b4 = Button(buttonFrame, text="Analyse", command=analyse)
 b4.pack()
 
 buttonFrame.pack()
+
+score_label = Label(master=root, text="")
+score_label.pack()
+
 mainloop()
