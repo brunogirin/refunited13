c143160c875dcbd309e00a4d3c526b0f19b5ecdf
James Porter <j.a.porter@warwick.ac.uk>
2013-01-19 16:34:59 +0000

    reordered gui

--- gui.py
+++ gui.py
@@ -40,20 +40,23 @@ label.pack()
 msg = Label(master=root, text=current_message)
 msg.pack(side=TOP, fill=X, expand=1)
 
-buttonFrame = Frame(master=root)
-
-b = Button(buttonFrame, text="Spam", command=spam)
-b.pack()
-b2 = Button(buttonFrame, text="Not Spam", command=notspam)
-b2.pack()
-b3 = Button(buttonFrame, text="Next", command=next)
-b3.pack()
-b4 = Button(buttonFrame, text="Analyse", command=analyse)
-b4.pack()
-
-buttonFrame.pack()
-
 score_label = Label(master=root, text="")
 score_label.pack()
 
+buttonFrame = Frame(master=root)
+
+b = Button(buttonFrame, text="Spam", command=spam)
+b.grid(row=0, column=0)
+b.pack()
+b2 = Button(buttonFrame, text="Not Spam", command=notspam)
+b2.grid(row=0, column=1)
+b2.pack()
+b3 = Button(buttonFrame, text="Next", command=next)
+b3.grid(row=0, column=2)
+b3.pack()
+b4 = Button(buttonFrame, text="Analyse", command=analyse)
+b4.grid(row=0, column=3)
+b4.pack()
+
+buttonFrame.pack()
 mainloop()
