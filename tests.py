d43343c135eaed12bd0f3c805aa39c85f7fddb47
James Porter <j.a.porter@warwick.ac.uk>
2013-01-19 13:29:28 +0000

    Adding (non-implemented) tests

--- tests.py
+++ tests.py
@@ -11,3 +11,16 @@ class TestApi(unittest.TestCase):
 
     def test_invalid_uri(self):
         self.assertEqual(404, self.api.get("wrong").status_code)
+
+    def test_view_messages(self):
+        pass
+
+    def test_send_message(self):
+        pass
+
+class TestSpam(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
